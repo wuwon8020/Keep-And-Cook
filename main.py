64140798c00cd316e3a7ec756df412c7e2434e9b
wuwon8020 <91567681+wuwon8020@users.noreply.github.com>
2026-06-02 06:32:45 +0000

    title 및 side

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ import streamlit as st
 import openai as OpenAI
 import kitchen
 import refrigator
+
+st.title("Keep And Cook")
+page = st.sidebar.radio(
+    "항목 선택",
+    (   "Receipt",
+        "Refrigator",
+        "Kitchen"
+    )
+)
