f3c97dccaf84dea3ef2be9ae981937a6a82c9a3d
wuwon8020 <91567681+wuwon8020@users.noreply.github.com>
2026-06-08 00:25:16 +0000

    main 페이지 연결

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import openai as OpenAI
 import Receipt
-import kitchen
-import refrigator
+import Kitchen
+import Refrigator
 
 st.title("Keep And Cook")
 page = st.sidebar.radio(
@@ -13,3 +13,17 @@ page = st.sidebar.radio(
         "Kitchen"
     )
 )
+if page == "api_key 입력":
+    Lab21_api.app()
+
+elif page == "api_key":
+    api_key.app()
+
+elif page == "Receipt":
+    Receipt.app()
+
+elif page == "Refrigator":
+    Refrigator.app()
+    
+elif page == "kitchen":
+    Kitchen.app()
