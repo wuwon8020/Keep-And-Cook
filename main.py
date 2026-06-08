9483c1ba4ab9665d243b2493c71f61040f0a79e4
wuwon8020 <91567681+wuwon8020@users.noreply.github.com>
2026-06-08 00:31:28 +0000

    a

--- main.py
+++ main.py
@@ -7,16 +7,14 @@ import Refrigator
 st.title("Keep And Cook")
 page = st.sidebar.radio(
     "항목 선택",
-    (   "main",
+    (   "api_key",
         "Receipt",
         "Refrigator",
         "Kitchen"
     )
 )
-if page == "api_key 입력":
-    Lab21_api.app()
 
-elif page == "api_key":
+if page == "api_key":
     api_key.app()
 
 elif page == "Receipt":
